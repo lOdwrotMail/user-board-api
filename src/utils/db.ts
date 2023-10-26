6c260779bbb54c30ea55820fecc7f54cea51dc52
lOdwrotMail <lukasz.o@mate.academy>
2023-10-26 23:59:29 +0200

    adjusted credentials

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,27 +1,27 @@
-import { Sequelize } from 'sequelize-typescript';
-import * as models from '../model';
-
+import { Sequelize } from "sequelize-typescript";
+import * as models from "../model";
 
 export const sequelize = new Sequelize({
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
-  dialect: 'postgres', 
+  port: Number(process.env.DB_PORT),
+  dialect: "postgres",
   models: Object.values(models),
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
-    }
-  }
+    },
+  },
 });
 
 export async function connect() {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 }
